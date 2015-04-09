abf8189f53436234395c51613723938620c95d9b
Eugene Nikolskyi <eugene.nikolskyi@lge.com>
2015-04-09 15:13:01 -0700

    Use ASIHTTPRequest library from CocoaPods

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '7.1'
+
+xcodeproj 'ConnectSDK'
+
+pod 'ASIHTTPRequest/Core', '~> 1.8.2'
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// ASIHTTPRequest/Core
+#define COCOAPODS_POD_AVAILABLE_ASIHTTPRequest_Core
+#define COCOAPODS_VERSION_MAJOR_ASIHTTPRequest_Core 1
+#define COCOAPODS_VERSION_MINOR_ASIHTTPRequest_Core 8
+#define COCOAPODS_VERSION_PATCH_ASIHTTPRequest_Core 2
+
+// Reachability
+#define COCOAPODS_POD_AVAILABLE_Reachability
+#define COCOAPODS_VERSION_MAJOR_Reachability 3
+#define COCOAPODS_VERSION_MINOR_Reachability 2
+#define COCOAPODS_VERSION_PATCH_Reachability 0
+
