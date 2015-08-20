ec36353dabfcad803d1c454e5377e37c5dc53dd8
Eugene Nikolskyi <eugene.nikolskyi@lge.com>
2015-08-19 17:25:51 -0700

    Create dynamic framework target for Carthage

---
+++ ConnectSDKDynamic/ConnectSDKDynamic.h
@@ -0,0 +1,19 @@
+//
+//  ConnectSDKDynamic.h
+//  ConnectSDKDynamic
+//
+//  Created by Eugene Nikolskyi on 2015-08-19.
+//  Copyright (c) 2015 LG Electronics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConnectSDKDynamic.
+FOUNDATION_EXPORT double ConnectSDKDynamicVersionNumber;
+
+//! Project version string for ConnectSDKDynamic.
+FOUNDATION_EXPORT const unsigned char ConnectSDKDynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConnectSDKDynamic/PublicHeader.h>
+
+
